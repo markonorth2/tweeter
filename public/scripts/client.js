7febaa8cb3ea550081ed585439afa95de3ebe23c
Mark Chuang <markchuangth@gmail.com>
2021-12-17 09:05:09 -0500

    cleaning up the code and did the code review with senior mentor

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const renderTweets = function(tweets) {
-  // loops through tweets
+const renderTweets = function (tweets) {
+  // grabbing all tweets and rendering each tweet individually
   for (let users of tweets) {
     // calls createTweetElement for each tweet
     const $tweet = createTweetElement(users);
-    
+
     // takes return value and appends it to the tweets container
-    $('#posted-tweets').prepend($tweet);
+    $("#posted-tweets").prepend($tweet);
   }
-  
-}
+};
 
 const escape = function (str) {
   let div = document.createElement("div");
@@ -22,14 +21,14 @@ const escape = function (str) {
   return div.innerHTML;
 };
 
-
-//takes in a tweet object and is responsible for returning a tweet <article> element containing the 
+//takes in a tweet object and is responsible for returning a tweet <article> element containing the
 //entire HTML structure of the tweet.
 
-const createTweetElement = function(tweetData){
+const createTweetElement = function (tweetData) {
+  const avatar = tweetData.user.avatars;
   const $tweet = $(`<article>
   <header>
-    <img src = "${tweetData.user.avatars}">
+    <img src = "${avatar}">
     <h4>${tweetData.user.name}</h4>
     <div>${tweetData.user.handle}</div>
   </header>
@@ -43,40 +42,36 @@ const createTweetElement = function(tweetData){
     </div>
   </footer>
 </article>`);
-return $tweet;
-}
+  return $tweet;
+};
 
-$(document).ready(function(){
-  function loadTweets(){
-    $.get("/tweets")
-    .then((data) => {
+$(document).ready(function () {
+  function loadTweets() {
+    $.get("/tweets").then((data) => {
       $("#posted-tweets").empty();
       renderTweets(data);
-      
-    })
+    });
   }
   loadTweets();
-  $("form.tweet-form").on("submit", function(event) {
+  $("form.tweet-form").on("submit", function (event) {
     event.preventDefault();
     const $data = $(this).serialize();
-    //console.log($data);
-    const $message = $("#tweet-text").val()
-    console.log($message.length);
-    if ($message === null || $message === ""){
-      $(".error-message").text("🛑Your message is too short or invalid!🛑").slideDown(400).slideUp(5000);
-    }
-    else if ($message.length > 140){
-      $(".error-message").text("🛑Your message is too long!🛑").slideDown(400).slideUp(5000);
-    }
-    else {
-      $.post("/tweets", $data)
-      .then(() => {
-        console.log("this is a message")
+    const $message = $("#tweet-text").val();
+    if ($message === null || $message === "") {
+      $(".error-message")
+        .text("🛑Your message is too short or invalid!🛑")
+        .slideDown(400)
+        .slideUp(5000);
+    } else if ($message.length > 140) {
+      $(".error-message")
+        .text("🛑Your message is too long!🛑")
+        .slideDown(400)
+        .slideUp(5000);
+    } else {
+      $.post("/tweets", $data).then(() => {
+        console.log("this is a message");
         loadTweets();
-      })
+      });
     }
-    
-  })
-  
-  
-})
+  });
+});
